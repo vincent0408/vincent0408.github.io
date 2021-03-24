4e10e9c98b570e1eb822bbfdad21bc2b9e8f967c
Vincent <tnecniv43208@gmail.com>
2021-03-24 15:10:10 +0800

    Update index.js

--- PWS final/js/index.js
+++ PWS final/js/index.js
@@ -3,6 +3,9 @@ var user = localStorage['User'];
 localStorage.removeItem('User');
 console.log(user)
 
+username_display = document.getElementsByClassName('mr-2 d-none d-lg-inline text-gray-600 small')[0]
+username_display.textContent = user
+
 firebase.database().ref().on('value', snapshot => {
     let snap = snapshot.val()
     var columns = Object.keys(snap[1])
